4b1c6608790b1556fcd24d0d1388e78d5804a576
Harika Nukala <harikanukala9@gmail.com>
2016-03-30 10:41:39 -0700

    Modified CreateDB script

--- src/configs/CreateDB.php
+++ src/configs/CreateDB.php
@@ -3,13 +3,13 @@ $link = mysqli_connect('localhost','root','yes');
 if (!$link) {
     die('Could not connect: ' . mysqli_connect_error());
 }
-
+mysqli_query($link,'DROP DATABASE IF EXISTS HW3');
 $sql = 'CREATE DATABASE HW3';
 if (mysqli_query($link,$sql)) {
     echo "Database HW3 created successfully\n";
     $db_selected=mysqli_select_db($link,'HW3');
     $sql='CREATE TABLE users(
-     user_id int(5) NOT NULL,
+     user_id int(5) NOT NULL AUTO_INCREMENT,
      user_name VARCHAR(50),
      password VARCHAR(50),
      PRIMARY KEY(user_id)
@@ -27,7 +27,7 @@ if (mysqli_query($link,$sql)) {
 	    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 	}
 	$sql='CREATE TABLE images(
-		image_id int(5) NOT NULL,
+		image_id int(5) NOT NULL AUTO_INCREMENT,
 		image_name VARCHAR(50),
 		image_caption VARCHAR(100),
 		avg_rating FLOAT,
@@ -49,11 +49,12 @@ if (mysqli_query($link,$sql)) {
 	    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
 	}
 	$sql='CREATE TABLE ratings(
-		id int(5) NOT NULL,
+		id int(5) NOT NULL AUTO_INCREMENT,
 		image_id int(5),
 		user_id int(5),
 		rate int(5),
 		rated_date TIMESTAMP,
+		PRIMARY KEY(id),
 		FOREIGN KEY (image_id) REFERENCES images(image_id),
 		FOREIGN KEY (user_id) REFERENCES users(user_id)
 		)';
